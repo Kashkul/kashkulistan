008ad2467d0e2444cd7503382a97b8140ccb4b0c
Roger <roger.e.clayton@gmail.com>
2017-10-25 15:24:06 -0700

    playing with navbar styling - need to break things into components

--- app/javascript/kashkulistan_client/pages/HomePage.vue
+++ app/javascript/kashkulistan_client/pages/HomePage.vue
@@ -1,8 +1,23 @@
 <template>
-	<p>Home is working!</p>
+	<div>
+		<nav class="navbar nav-is-fixed is-dark">
+	    <div class="navbar-brand">
+    		<a class="navbar-item" href="https://bulma.io">
+     			<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+    		</a>
+  		</div>
+		</nav>
+		<section class="hero is-fullheight is-light">
+			
+		</section>
+		<section class="hero is-fullheight is-primary">
+			<p class="title">Home is working!</p>
+		</section>
+	</div>
 </template>
 
 <script>
+
 export default {
 
   name: 'HomePage',
@@ -10,10 +25,19 @@ export default {
   data () {
     return {
 
-    };
+    }
   }
-};
+}
+
 </script>
 
-<style lang="css" scoped>
+<style lang="sass" scoped>
+@import '~bulma'
+
+.nav-is-fixed
+  top: 0
+  z-index: 100
+  position: fixed
+  width: 100%
+
 </style>

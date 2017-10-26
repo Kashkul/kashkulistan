6872e20fc0ae5f144418ba9fe1333c219e02ce1e
Roger <roger.e.clayton@gmail.com>
2017-10-26 14:49:21 -0700

    adds title to hero

--- app/javascript/kashkulistan_client/pages/HomePage.vue
+++ app/javascript/kashkulistan_client/pages/HomePage.vue
@@ -1,20 +1,24 @@
 <template>
 	<div>
 		<nav class="navbar nav-is-fixed is-dark">
-	    <div class="navbar-brand">
-    		<a class="navbar-item" href="https://bulma.io">
-     			<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-    		</a>
-  		</div>
+			<div class="navbar-brand">
+				<a class="navbar-item" href="https://bulma.io">
+					<img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+				</a>
+			</div>
 		</nav>
 		<parallax :fixed="true">
-			<section class="hero is-fullheight is-light">
-				<p>STuff</p>
-				<p>STuff</p>
-				<p>STuff</p>
-				<p>STuff</p>
-				<p>STuff</p>
-				<p>STuff</p>
+			<section class="hero is-fullheight is-dark is-bold">
+				<div class="hero-body">
+					<div class="container has-text-centered">
+						<h1 class="title is-1">
+							Kashkul:
+						</h1>
+						<h2 class="title">
+							1. First definition goes here, this will eventually animate to include the other definitions. Bla bla bla. Also more stuff.
+						</h2>
+					</div>
+				</div>
 			</section>
 		</parallax>
 		<section class="hero is-fullheight is-primary">

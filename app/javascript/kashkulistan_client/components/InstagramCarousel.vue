1c7982ad95ec2d091b12e07d2adaa599f8d8974d
Roger <roger.e.clayton@gmail.com>
2017-10-26 23:34:57 -0700

    adds axios, empty InstagramCarousel component

---
+++ app/javascript/kashkulistan_client/components/InstagramCarousel.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'InstagramCarousel',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>

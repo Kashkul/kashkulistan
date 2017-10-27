973ed1f57a9c0c2e1e9f5c2119455de28f7f73d1
Roger <roger.e.clayton@gmail.com>
2017-10-26 23:32:40 -0700

    adds vue-carousel - still experimenting with instafeed

--- app/javascript/kashkulistan_client/pages/HomePage.vue
+++ app/javascript/kashkulistan_client/pages/HomePage.vue
@@ -9,7 +9,10 @@
 				<div class="container has-text-centered">
 					<h1 class="subtitle is-3">
 						<strong>This is our kashkul.</strong>
-						<div id="instafeed"></div>
+						
+						<carousel>
+							<div id="instafeed"></div>
+						</carousel>
 					</h1>
 				</div>
 			</div>
@@ -23,24 +26,27 @@ import Parallax from 'vue-parallaxy'
 import NavBar from '../components/NavBar.vue'
 import SplashScreen from '../components/SplashScreen.vue'
 import AboutSection from '../components/AboutSection.vue'
-import * as Instafeed from 'instafeed.js'
-
- var feed = new Instafeed({
-        get: 'user',
-        userId: 953380486,
-        clientId: process.env.INSTAGRAM_CLIENT_ID,
-        accessToken: process.env.ROGER_INSTAGRAM_ACCESS_TOKEN
-    });
-    feed.run();
-    console.log(feed)
+import Instafeed from 'instafeed.js'
+import {Carousel, Slide} from 'vue-carousel'
 
 export default {
   name: 'HomePage',
-  components: {Parallax, NavBar, SplashScreen, AboutSection},
+  components: {Parallax, NavBar, SplashScreen, AboutSection, Carousel, Slide},
   data () {
     return {
 
     }
+  },
+  beforeCreate() {
+  	 var feed = new Instafeed({
+        get: 'user',
+        userId: 953380486,
+        clientId: process.env.INSTAGRAM_CLIENT_ID,
+        accessToken: process.env.ROGER_INSTAGRAM_ACCESS_TOKEN,
+        template: '<slide><a href="{{link}}"><img src="{{image}}" /></a></slide>'
+    });
+    console.log(feed._buildUrl)
+  	 feed.run()
   }
 }
 

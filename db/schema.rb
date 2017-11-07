dab0333970b4dc91260d92a6eb1183939f613c6b
Roger <roger.e.clayton@gmail.com>
2017-11-06 17:49:32 -0800

    adds dummy people seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171107010200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
+    t.text "about"
+    t.string "facebook"
+    t.string "twitter"
+    t.string "instagram"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+12.times do 
+	Person.create!(name: 'Roger Clayton', role: 'Does Stuff', about: 'Savan A. Ahmad is an undergraduate in the Administration and Economic Department at the University of Sulaimani where she majors in Accounting. Ahmad is the Translation Supervisor at DidiMn as well as a member of 68, a youth organization. She also subtitles movies for Salim cinema. She participated in gallery shows for her paintings at a very young age and continues to draw and paint today. She currently works as a translator at Kashkul where she focuses on bringing the fifteen major Kurdish classical poets of the 19th century into English, an on-going project.', facebook: 'http://facebook.com', twitter: 'http://twitter.com', instagram: 'http://instagram.com')
+end

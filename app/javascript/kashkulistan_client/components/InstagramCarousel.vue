d3079493443b05f2e697891a10bd5a194852ad33
Roger <roger.e.clayton@gmail.com>
2017-11-14 20:12:10 -0800

    uses only proper sized insta dummy photos

--- app/javascript/kashkulistan_client/components/InstagramCarousel.vue
+++ app/javascript/kashkulistan_client/components/InstagramCarousel.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="carousel-container">
   	<carousel  :autoplay="true" :perPage="2">
-  		<slide class="slide"><img src="assets/instastatic/1.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/2.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/3.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/4.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/2.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/3.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/4.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/2.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/3.png"></slide>
+      <slide class="slide"><img src="assets/instastatic/4.png"></slide>
   		<slide class="slide"><img src="assets/instastatic/2.png"></slide>
   		<slide class="slide"><img src="assets/instastatic/3.png"></slide>
   		<slide class="slide"><img src="assets/instastatic/4.png"></slide>

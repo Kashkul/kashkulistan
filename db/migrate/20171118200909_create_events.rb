f3606fcde9c412ce5e92b428c4a5ba9f0d0985a4
Roger <roger.e.clayton@gmail.com>
2017-11-18 12:20:35 -0800

    adds dummy seeds for events

--- db/migrate/20171118200909_create_events.rb
+++ db/migrate/20171118200909_create_events.rb
@@ -2,7 +2,7 @@ class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
       t.string :name
-      t.date :date
+      t.string :date
       t.text :description
       t.string :url
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107010200) do
+ActiveRecord::Schema.define(version: 20171118200909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "date"
+    t.text "description"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"

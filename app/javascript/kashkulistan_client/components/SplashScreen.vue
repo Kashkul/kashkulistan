754c8703c8fe60606269a99fe4978804ca69ba41
Roger <roger.e.clayton@gmail.com>
2017-11-24 11:09:30 -0500

    using scroll reveal on heading so it will hide after scroll

--- app/javascript/kashkulistan_client/components/SplashScreen.vue
+++ app/javascript/kashkulistan_client/components/SplashScreen.vue
@@ -2,12 +2,14 @@
 	<section class="hero is-fullheight is-dark is-bold">
 		<div class="hero-body">
 			<div class="container has-text-centered">
-				<h1 class="title is-1">
-					Kashkul:
-				</h1>
-				<h2 class="title is-4">
-					3. a research, translation, and preservation collaborative based in Sulaimani, Iraq. Iraqi, Kurdish, and American scholars come together to pursue projects of their own design. All members of the collaborative hope that this is our work, in the digital world: to create, curate, and preserve culture in the northern regions of Iraq.
-				</h2>
+				<section v-scroll-reveal.reset>
+					<h1 class="title is-1">
+						Kashkul:
+					</h1>
+					<h2 class="title is-4">
+						3. a research, translation, and preservation collaborative based in Sulaimani, Iraq. Iraqi, Kurdish, and American scholars come together to pursue projects of their own design. All members of the collaborative hope that this is our work, in the digital world: to create, curate, and preserve culture in the northern regions of Iraq.
+					</h2>
+				</section>
 			</div>
 		</div>
 	</section>

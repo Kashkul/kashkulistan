39eae95785c26dbf8f6b7aac0f884584e0057cad
Roger <roger.e.clayton@gmail.com>
2017-11-24 16:27:05 -0500

    adds more filler text for dummy project tile

--- app/javascript/kashkulistan_client/components/ProjectTile.vue
+++ app/javascript/kashkulistan_client/components/ProjectTile.vue
@@ -5,8 +5,10 @@
 				<div class="tile is-parent">
 					<article class="tile is-child box notification is-dark">
 						<section class="hero">
-							<div class="hero-body">
+							<div class="hero-body content">
 								<p class="title">Stuff</p>
+								<p class="Subtitle">More stuff</p>
+								<p>Full description and stuff about the stuff that happened. Maybe talk about who was responsible for making the stuff happen, and who was brought in to talk about the stuff. Any particularly cool highlights about the stuff would probably also be discussed here. I wonder what might be able to be done with the stuff in the future... Maybe if you want to read some more about what we did with the stuff you can look at this link.</p>
 							</div>
 						</section>
 					</article>			

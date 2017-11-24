879892520304ee7f0f9bdea68e0e136084a929e2
Roger <roger.e.clayton@gmail.com>
2017-11-24 17:47:51 -0500

    changes rows to 3

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PeopleController < ApplicationController
 	def index
 		#This will return a collection of all people in the database, divided into pairs.
 		#The pair grouping allows the front end to easily arrange them in the tile layout.
-		@people = Person.make_rows(2)
+		@people = Person.make_rows(3)
 		render json: @people
 	end
 end

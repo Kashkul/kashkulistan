55632afd1e234fa5f3a16c2b377ec7bcba1d6dd1
Roger <roger.e.clayton@gmail.com>
2017-12-08 20:24:24 -0800

    makes hero fullheight for spacing

--- app/javascript/kashkulistan_client/components/AboutSection.vue
+++ app/javascript/kashkulistan_client/components/AboutSection.vue
@@ -1,5 +1,5 @@
 <template>
-	<section class="hero is-large is-white">
+	<section class="hero is-fullheight is-white">
 		<div class="hero-body">
 			<div class="container has-text-centered" v-scroll-reveal.reset>
 				<p class="title">"Possible content here. Maybe something that's not too long about Kashkul. Now I'm just typing some stuff to make this a little longer. Maybe it's a quote?"</p>

b9d534ad8392e9e25500ddd85dba9c2fac36a846
Roger <roger.e.clayton@gmail.com>
2017-12-08 21:52:08 -0800

    adjusts dummy text to reflect content expectations

--- app/javascript/kashkulistan_client/components/ProjectTile.vue
+++ app/javascript/kashkulistan_client/components/ProjectTile.vue
@@ -7,8 +7,7 @@
 						<section class="hero">
 							<div class="hero-body content">
 								<p class="title">Stuff</p>
-								<p class="Subtitle">More stuff</p>
-								<p>Full description and stuff about the stuff that happened. Maybe talk about who was responsible for making the stuff happen, and who was brought in to talk about the stuff. Any particularly cool highlights about the stuff would probably also be discussed here. I wonder what might be able to be done with the stuff in the future... Maybe if you want to read some more about what we did with the stuff you can look at this link.</p>
+								<p class="Subtitle">More stuff about the event. Use the short one line description. Maybe it's a couplefew sentences?</p>
 							</div>
 						</section>
 					</article>			

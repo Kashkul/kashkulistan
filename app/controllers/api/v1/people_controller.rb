a9eeb050cf2120b0ae568de4f739d0b4ebccd32b
Roger <roger.e.clayton@gmail.com>
2017-12-13 11:16:50 -0800

    updates comment to accurately reflect code

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PeopleController < ApplicationController
 	def index
-		#This will return a collection of all people in the database, divided into pairs.
-		#The pair grouping allows the front end to easily arrange them in the tile layout.
+		#This will return a collection of all people in the database, divided into threes.
+		#The triple grouping allows the front end to easily arrange them in the tile layout.
 		@people = Person.make_rows(3)
 		render json: @people
 	end

993cf8af6445e264a43a7a0ce412b9852e05f4cc
Roger <roger.e.clayton@gmail.com>
2017-12-13 11:17:21 -0800

    adds index route for projects api route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   	namespace :v1 do
     	resources :people, only: [ :index ]
     	resources :events, only: [ :index ]
+    	resources :projects, only: [ :index ]
     end
   end
 end  

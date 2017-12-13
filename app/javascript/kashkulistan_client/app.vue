73536e74a5af8d3011188234d4375b916cfc5adf
Roger <roger.e.clayton@gmail.com>
2017-12-13 10:55:13 -0800

    font changes now importing properly

--- app/javascript/kashkulistan_client/app.vue
+++ app/javascript/kashkulistan_client/app.vue
@@ -30,7 +30,7 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-  @import '~bulma'
+  @import './styles/app.sass'
 
   div
     font-family: $family-primary
--- app/javascript/kashkulistan_client/components/AboutSection.vue
+++ app/javascript/kashkulistan_client/components/AboutSection.vue
@@ -2,7 +2,7 @@
 	<section class="hero is-large is-white">
 		<div class="hero-body">
 			<div class="container has-text-centered" v-scroll-reveal.reset>
-				<p class="title klala">"Possible content here. Maybe something that's not too long about Kashkul. Now I'm just typing some stuff to make this a little longer. Maybe it's a quote?"</p>
+				<p class="title">"Possible content here. Maybe something that's not too long about Kashkul. Now I'm just typing some stuff to make this a little longer. Maybe it's a quote?"</p>
 			</div>
 		</div>
 	</section>
@@ -21,8 +21,5 @@
 
 <style lang="sass" scoped>
 	@import '../styles/app.sass'
-
-	.klala
-		font-family: $family-primary
 	
 </style>

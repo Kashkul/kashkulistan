64267187d9c87e10384ad4ee1790701dd33603f6
Roger <roger.e.clayton@gmail.com>
2017-12-18 11:46:52 -0800

    adds create method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,36 @@
 class ProjectsController < ApplicationController
+
+	def index
+		@projects = Project.all
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(project_params)
+
+		if @project.save
+			flash[:success] = "Project saved!"
+		else
+			render 'new'
+		end
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def project_params
+		params.require(:project).permit(:name, :description, :authors, :url, :image)
+	end
+
+	def find_project
+	end
+
 end

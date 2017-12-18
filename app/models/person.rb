a4d0e7b50d104e5c5dff55e505b90970a25a9314
Roger <roger.e.clayton@gmail.com>
2017-12-17 20:40:38 -0800

    changes spacing

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
+
 	#make_rows takes all instances of people in the database and organizes them into groups of designated size, 
 	#which allows the front-end to easily format them into rows using the tile format.
 	#TODO: refactor this/projects method to make it DRY.
@@ -13,4 +14,5 @@ class Person < ApplicationRecord
 		#split people_array into subarrays with groups, size of which designated by the argument passed as the number param
 		grouped_array = people_array.each_slice(number).to_a
 	end
+	
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+
 	#This functions in the same way as the make_rows method for people - It groups the projects into segments easily formatted by the vue app.
 	#TODO: refactor this/people method to make it DRY.
 	def self.make_rows(number)
@@ -12,4 +13,5 @@ class Project < ApplicationRecord
 		#split projects_array into subarrays with groups, size of which designated by the argument passed as the number param
 		grouped_array = projects_array.each_slice(number).to_a
 	end
+
 end

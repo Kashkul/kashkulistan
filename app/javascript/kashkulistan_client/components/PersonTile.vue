c7cd97aa79bc7d0c41501722305ad53c9fc8aaf7
Roger <roger.e.clayton@gmail.com>
2018-02-28 14:07:38 -0800

    removes social media

--- app/javascript/kashkulistan_client/components/PersonTile.vue
+++ app/javascript/kashkulistan_client/components/PersonTile.vue
@@ -5,17 +5,6 @@
 				<div class="content">
 					<p class="title">{{tileData.name}}</p>
 					<p class="subtitle">{{tileData.role}}</p>
-					<div class="social-media-icons">
-						<span class="icon has-text-info">
-							<i class="fa fa-twitter" @click="openSocialMedia('twitter')"></i>
-						</span>
-						<span class="icon has-text-info">
-							<i class="fa fa-facebook" @click="openSocialMedia('facebook')"></i>
-						</span>					
-						<span class="icon has-text-info">
-							<i class="fa fa-instagram" @click="openSocialMedia('instagram')"></i>
-						</span>
-					</div>
 					<p>{{tileData.about}}</p>
 				</div>
 			</section>
@@ -31,27 +20,7 @@
 	    return {
 
 	    }
-	  },
-	  methods: {
-	  	//TO-DO: refactor without switch statement. Don't really need a method here at all - just use hrefs
-	  	openSocialMedia(platform) {
-	  		var url
-	  		//determines which social media platform url to use
-	  		switch (platform) {
-	  			case 'twitter':
-	  				url = this.tileData.twitter
-	  				break
-	  			case 'facebook':
-	  				url = this.tileData.facebook
-	  				break
-	  			case 'instagram':
-	  				url = this.tileData.instagram
-	  		}
-	  		//opens url in new tab
-        let win = window.open(url, '_blank')
-        win.focus()
-      }
-    }
+	  }
 	}
 </script>
 

54ca88b50404a27e61a13f82ac76b3bd4521820e
Roger <roger.e.clayton@gmail.com>
2018-03-22 17:31:19 -0600

    adds methods for grouping people by category

--- app/models/person.rb
+++ app/models/person.rb
@@ -16,5 +16,29 @@ class Person < ApplicationRecord
 		#split people_array into subarrays with groups, size of which designated by the argument passed as the number param
 		grouped_array = people_array.each_slice(number).to_a
 	end
-	
+
+	def self.group_by_category(array_of_categories)
+		#define empty array for all people
+		people_array = []
+		#Loop through inputted categories, making an array for each and adding it to the people_array
+		array_of_categories.each do |category|
+			people_array << make_categorized_array(category)
+		end
+		return people_array
+	end
+
+	private
+	def self.make_categorized_array(category)
+		categorized_array = []
+		category_members = find_by_category(category)
+		category_members.each do |member|
+			categorized_array << member
+		end
+		return categorized_array
+	end
+
+	def self.find_by_category(category)
+		category_members = self.where(category: category)
+		return category_members
+	end
 end

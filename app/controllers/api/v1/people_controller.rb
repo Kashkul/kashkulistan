a0135df5c0a316372f95cec79ebd90b1e395b0d6
Roger <roger.e.clayton@gmail.com>
2018-03-22 21:03:38 -0600

    comment revision to reflect method swap

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::PeopleController < ApplicationController
 	def index
-		#This will return a collection of all people in the database, divided into threes.
-		#The triple grouping allows the front end to easily arrange them in the tile layout.
+		#This returns all people in the database, grouped by assigned category.
 		@people = Person.group_by_category(['Contributor', 'Staff', 'Director'])
 		render json: @people
 	end

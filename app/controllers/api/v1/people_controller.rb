c16b938dc2fcfb88791b0a1e6d3db5c222070010
Roger <roger.e.clayton@gmail.com>
2018-03-25 13:02:01 -0600

    updates category names

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::PeopleController < ApplicationController
 	def index
 		#This returns all people in the database, grouped by assigned category.
-		@people = Person.group_by_category(['Contributor', 'Staff', 'Director'])
+		@people = Person.group_by_category(['Researcher', 'Visiting', 'Director'])
 		render json: @people
 	end
 end

3eaebe8ac16b70954533ad8ee6d0d1935e8c3851
Roger <roger.e.clayton@gmail.com>
2018-03-26 12:18:32 -0600

    upgrades webpacker, adds logo svg, starts playing with splash page formatting

--- app/javascript/kashkulistan_client/components/SplashScreen.vue
+++ app/javascript/kashkulistan_client/components/SplashScreen.vue
@@ -1,23 +1,36 @@
 <template>
 	<section class="hero is-fullheight is-black is-bold">
 		<div class="hero-body">
-			<div class="container has-text-centered">
+			<div class="svg-container">
+				<kashkul-logo></kashkul-logo>
+			</div>
+			<div class="container">
 				<section v-scroll-reveal.reset>
-					<h1 class="title is-1">
-						Kashkul:
-					</h1>
-					<h2 class="title is-4">
-						3. a research, translation, and preservation collaborative based in Sulaimani, Iraq. Iraqi, Kurdish, and American scholars come together to pursue projects of their own design. All members of the collaborative hope that this is our work, in the digital world: to create, curate, and preserve culture in the northern regions of Iraq.
-					</h2>
+						<h1 class="title is-2">
+							Kashkul is a research, translation, and preservation collaborative based in Sulaimani, Iraq. Iraqi, Kurdish, and American scholars come together to pursue projects of their own design. All members of the collaborative hope that this is our work, in the digital world: to create, curate, and preserve culture in the northern regions of Iraq.
+						</h1>
+						<h2 class="title is-3">
+							
+						</h2>
 				</section>
+			</div>
+		</div>
+		<div class="hero-footer">
+			<div class="container has-text-centered">
+				<h1 class="title is-3">
+					This is our Kashkul.
+				</h1>
 			</div>
 		</div>
 	</section>
 </template>
 
 <script>
+	import KashkulLogo from './KashkulLogo.vue'
+
 	export default {
 	  name: 'SplashScreen',
+	  components: { KashkulLogo },
 	  data () {
 	    return {
 
@@ -28,5 +41,14 @@
 
 <style lang="sass" scoped>
 	@import '../styles/app.sass'
+
+	.svg-container
+		height: 50%
+		width: 50%
+		margin: auto
+		z-index: 0
+
+	.hero-footer
+		margin-bottom: 50px
 
 </style>
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
+const vue =  require('./loaders/vue')
+
+const fileLoader = environment.loaders.get('file')
+
+environment.loaders.append('vue', vue)
 
 module.exports = environment

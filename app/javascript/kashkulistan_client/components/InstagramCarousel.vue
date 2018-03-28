9e077154292f8c30f6d7aaecfc88ab2adc154b1b
Roger <roger.e.clayton@gmail.com>
2018-03-27 18:11:00 -0600

    pulling Kashkul insta data - figuring out how to use it

--- app/javascript/kashkulistan_client/components/InstagramCarousel.vue
+++ app/javascript/kashkulistan_client/components/InstagramCarousel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="carousel-container">
-  	<carousel  :autoplay="true" :perPage="2">
+  	<carousel  :autoplay="true" :loop="true" :perPage="2">
       <slide class="slide"><img src="assets/instastatic/2.png"></slide>
       <slide class="slide"><img src="assets/instastatic/3.png"></slide>
       <slide class="slide"><img src="assets/instastatic/4.png"></slide>
@@ -18,7 +18,7 @@
 </template>
 
 <script>
-  import axios from 'axios'
+  import jsonp from 'jsonp'
   import {Carousel, Slide} from 'vue-carousel'
 
   export default {
@@ -28,6 +28,27 @@
       return {
       	photos: []
       }
+    },
+    methods: {
+      getInstaData: function () {
+        let accessToken = process.env.KASHKUL_INSTAGRAM_ACCESS_TOKEN
+        console.log(accessToken)
+        let userId = process.env.KASHKUL_INSTAGRAM_ACCT_ID
+        let instagramApiUrl= `http://api.instagram.com/v1/users/${userId}/media/recent/?access_token=${accessToken}`
+
+        jsonp(instagramApiUrl, null, function (err, data) {
+          if (err) {
+            console.log(err.message)
+          } else {
+            console.log('You are in getInsta')
+            return data.data
+          }
+        })
+      }     
+    },
+    created() {
+      this.photos = this.getInstaData()
+      console.log(this.photos)
     }
   }
 </script>

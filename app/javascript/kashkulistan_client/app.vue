5aa65a77eaa5814d23b87239903e9fe796f1b354
Roger <roger.e.clayton@gmail.com>
2018-03-30 16:34:30 -0600

    adds v-cloak

--- app/javascript/kashkulistan_client/app.vue
+++ app/javascript/kashkulistan_client/app.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <nav-bar class="nav-is-fixed"></nav-bar>
-    <router-view id="main-content"></router-view>
-    <footer class="footer">
-      <div class="container">
-        <div class="content has-text-centered">
-          <p>
-            <strong>Kashkul</strong> by <a href="http://jgthms.com">Stuff</a>. The source code is licensed
-            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-          </p>
+    <div v-cloak>
+      <nav-bar class="nav-is-fixed"></nav-bar>
+      <router-view id="main-content"></router-view>
+      <footer class="footer">
+        <div class="container">
+          <div class="content has-text-centered">
+            <p>
+              <strong>Kashkul</strong> by <a href="http://jgthms.com">Stuff</a>. The source code is licensed
+              <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+              is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            </p>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   </div>
 </template>
 

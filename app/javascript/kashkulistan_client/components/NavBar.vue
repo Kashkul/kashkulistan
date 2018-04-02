d5c32bcf20497c7eaf9de591c2e6cac042ddb930
Roger <roger.e.clayton@gmail.com>
2018-04-02 14:30:50 -0600

    adds kashkul logo image

--- app/javascript/kashkulistan_client/components/NavBar.vue
+++ app/javascript/kashkulistan_client/components/NavBar.vue
@@ -2,15 +2,18 @@
 	<section> 
 		<nav class="navbar is-black">
 			<div class="navbar-brand">
-				<router-link to="/" class="navbar-item">Kashkul</router-link>
-			</div>
-			<div>
+					<router-link to="/" class="navbar-item" ><img id="circle-logo" src="../images/Kashkul-circlelogo.jpg" alt=""></router-link>
 			</div>
 			<div class="navbar-menu">
 				<div class="navbar-start">
 					<div class="navbar-item">
-						<router-link to="People">Our people</router-link>
+						<router-link to="People">People</router-link>
+					</div>							
+					<div class="navbar-item">
+						<router-link to="People">Projects</router-link>
 					</div>		
+				</div>
+				<div class="navbar-end">
 				</div>
 			</div>
 		</nav>
@@ -31,5 +34,8 @@
 
 <style lang="sass" scoped>
 	@import '../styles/app.sass'
+
+	#circle-logo
+		border-radius: 50%
 
 </style>

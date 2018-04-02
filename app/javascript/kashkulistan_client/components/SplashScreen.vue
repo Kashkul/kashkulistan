a9cf1063ce05395e10408c782a95cf0e28ef329a
Roger <roger.e.clayton@gmail.com>
2018-04-02 14:54:10 -0600

    testing without hero image

--- app/javascript/kashkulistan_client/components/SplashScreen.vue
+++ app/javascript/kashkulistan_client/components/SplashScreen.vue
@@ -101,7 +101,7 @@
 		position:
 
 	.splash-img
-		background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.8)), url('../images/cover.jpg')
+		background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.8))
 		width: 100%
 		height: 100%
 		background-size: cover

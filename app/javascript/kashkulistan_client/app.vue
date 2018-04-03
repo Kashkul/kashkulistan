485848f485a60ae8985a4b8618ae20fe1355c54c
Roger <roger.e.clayton@gmail.com>
2018-04-03 17:50:46 -0600

    adds copyright notice

--- app/javascript/kashkulistan_client/app.vue
+++ app/javascript/kashkulistan_client/app.vue
@@ -7,9 +7,7 @@
         <div class="container">
           <div class="content has-text-centered">
             <p>
-              <strong>Kashkul</strong> by <a href="http://jgthms.com">Stuff</a>. The source code is licensed
-              <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-              is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+              Copyright © 2018 Kashkul. All rights reserved.
             </p>
           </div>
         </div>
